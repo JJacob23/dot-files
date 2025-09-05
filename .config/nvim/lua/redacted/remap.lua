03207e08c8c7b39328a6e3f4b8f9022f54bf2f5b
Redacted <jjacob23@gmu.edu>
2025-09-05 15:48:04 -0400

    Adding hypr setup

--- .config/nvim/lua/redacted/remap.lua
+++ .config/nvim/lua/redacted/remap.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+local function run_and_copy(cmd)
+  local output = vim.fn.system(cmd)
+  vim.fn.setreg('+', output)
+  print("Output copied to clipboard:\n" .. output)
+end
+vim.keymap.set('n', '<leader>cc', function() run_and_copy('cargo check') end, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>cr', function() run_and_copy('cargo run') end, { noremap = true, silent = true })
+
